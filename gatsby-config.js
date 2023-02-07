d5f4f4934ad4bd8488facc3edc88b11ec63138f5
Michael Watchmaker <michaelwatchmaker@outlook.com>
2023-02-07 10:11:22 -0800

    Work from weekend

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.michaelwatchmaker.com",
     title: "Portfolio",
   },
   plugins: [
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,40 +1,22 @@
-import React, { useState }from "react";
+import React from "react";
 import { Link } from "gatsby";
 import Button from "./button";
 import "../styles/nav.css";
-import menu from "../images/icon-menu.svg";
-import close from "../images/icon-close.svg";
+import ArrowExternal from "../images/ArrowExternal";
+import Logo from "../images/Logo";
 
 const Navigation = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
-    const hide = () => setIsOpen(false);
-    const show = () => setIsOpen(true);
-  
-    const navigation = [
-      { link: '/about', text: 'About' },
-      { link: '/project', text: 'Project' },
-    ];
-  
     return (
       <nav>
-        <header>
-          <Link to="/" className="logo">Michael Watchmaker</Link>
-          <Button variant="alt icon" onClick={toggle}>
-            <img alt="icon" src={isOpen ? close : menu}/>
-          </Button>
-        </header>
-        {isOpen && (
-          <ul className="nav-links">
-            {navigation.map(nav => (
-              <li key={nav.text}>
-                <Link to={nav.link} onClick={toggle} onBlur={hide} onFocus={show}>
-                  {nav.text}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
+        <Link to="/" className="logo">
+            <Logo/>
+        </Link>
+        <div className="nav-links">
+          <Link to="/about">
+            <Button variant="button ghost">About</Button>
+          </Link>
+            <Button variant="button alt">Resume<ArrowExternal/></Button>
+        </div>
       </nav>
     );
   };

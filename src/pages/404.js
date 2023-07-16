827fbbfc308f71e51deba9e25ab702b548d80017
Michael Watchmaker <michaelwatchmaker@outlook.com>
2023-07-16 14:23:33 -0700

    Added 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,52 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Button from "../components/button"
+import Colorizer from "../components/colorizer"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="notFound">
+      <Colorizer
+        hex= '#1c1c1e'
+      />
+      <title>Michael Watchmaker</title>
+      <h1 className="center">🔭</h1>
+      <p className="body center text-dark-alt">We searched the galaxy, but couldn't find this page.</p>
+      <Link to="/">
+        <Button variant="button white-alt">
+          Back home
+        </Button>
+      </Link>
     </main>
   )
 }

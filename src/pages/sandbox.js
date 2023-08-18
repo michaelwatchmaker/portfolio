15a038956181709a31cf5021e02ad95a62537a3c
Michael Watchmaker <michaelwatchmaker@outlook.com>
2023-08-18 13:36:06 -0700

    Updating image routes

--- src/pages/sandbox.js
+++ src/pages/sandbox.js
@@ -6,18 +6,18 @@ import Colorizer from "../components/colorizer";
 import Navigation from "../components/nav";
 import Footer from "../components/footer";
 
-import ThumbnailWEBM from "../images/sandbox/thumbnail.webm";
-import MapWEBM from "../images/sandbox/map.webm";
-import TalkWEBM from "../images/sandbox/talk.webm";
-import SkeletonWEBM from "../images/sandbox/skeleton.webm";
+import ThumbnailWEBM from "../images/Sandbox/thumbnail.webm";
+import MapWEBM from "../images/Sandbox/map.webm";
+import TalkWEBM from "../images/Sandbox/talk.webm";
+import SkeletonWEBM from "../images/Sandbox/skeleton.webm";
 
-import ThumbnailMP4 from "../images/sandbox/thumbnail.mp4";
-import MapMP4 from "../images/sandbox/map.mp4";
-import TalkMP4 from "../images/sandbox/talk.mp4";
-import SkeletonMP4 from "../images/sandbox/skeleton.mp4";
+import ThumbnailMP4 from "../images/Sandbox/thumbnail.mp4";
+import MapMP4 from "../images/Sandbox/map.mp4";
+import TalkMP4 from "../images/Sandbox/talk.mp4";
+import SkeletonMP4 from "../images/Sandbox/skeleton.mp4";
 
-import Triage from "../images/sandbox/triage.png";
-import Icons from "../images/sandbox/icons.png";
+import Triage from "../images/Sandbox/triage.png";
+import Icons from "../images/Sandbox/icons.png";
 
 const ProjSandbox = () => {
   return (
